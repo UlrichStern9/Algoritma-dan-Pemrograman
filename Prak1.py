74d6da6bee3ca6afb5d2d3d079746e463a763c72
Muhammad Azka Aulia <88353426+UlrichStern9@users.noreply.github.com>
2021-09-26 01:37:27 +0700

    Update Prak1.py

--- Prak1.py
+++ Prak1.py
@@ -6,6 +6,7 @@ Jalankan file-nya, kemudian pilih sesuai elemen kompetensinya.
 Terima kasih.
 """
 
+
 def mulai():
     while True:
         masuk = int(input("Elkom?: "))
@@ -17,6 +18,7 @@ def mulai():
             elkom3()
         else:
             print("Pilih 1, 2 atau 3\n")
+
 
 def elkom1():
     print("        @            @@@@@@@@@@        @    @             @    ")
@@ -32,6 +34,7 @@ def elkom1():
     print('Ayah minggu lalu berkata, "Ayah besok akan pergi dinas"')
     print("I'm sitting on the bench\n")
 
+
 def elkom2():
     a1 = int(input("Masukkan titik koordinat A1: "))
     a2 = int(input("Masukkan titik koordinat A2: "))
@@ -39,6 +42,7 @@ def elkom2():
     b2 = int(input("Masukkan titik koordinat B2: "))
     jarak = ((((a2 - a1) ** 2) + ((b2 - b1) ** 2)) ** 0.5)
     print("\nJarak dari titik", (a1, a2), "dan", (b1, b2), "adalah: ", jarak, "\n")
+
 
 def elkom3():
     kecil = str(input("Please enter a 4-characters string: "))
@@ -49,6 +53,7 @@ def elkom3():
             besar = besar + chr(ord(i) - 32)
 
     print("The string capitalizaiton is: ", besar, "\n")
+
 
 if __name__ == "__main__":
     mulai()
