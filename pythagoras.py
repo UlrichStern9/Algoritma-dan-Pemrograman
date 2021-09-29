fcc25dce65fa1e1bdd4b20b86358a12e79fc4727
Muhammad Azka Aulia <88353426+UlrichStern9@users.noreply.github.com>
2021-09-29 10:51:45 +0700

    Add files via upload

---
+++ pythagoras.py
@@ -0,0 +1,31 @@
+from math import sqrt
+
+sisi = input("Sisi mana yang Anda ingin hitung? (a,b,c): ")
+
+if sisi == "c":
+    a = int(input("Masukkan panjang a: "))
+    b = int(input("Masukkan panjang b: "))
+    c = sqrt(a * a + b * b)
+    print("Panjang sisi c adalah:", round(c))
+
+elif sisi == "a":
+    b = int(input("Masukkan panjang b: "))
+    c = int(input("Masukkan panjang c: "))
+    if (c < b):
+        print("Panjang c tidak valid!")
+        c = int(input("Masukkan panjang c: "))
+
+    a = sqrt(c * c - b * b)
+    print("Panjang sisi a adalah:", round(a))
+
+elif sisi == "b":
+    a = int(input("Masukkan panjang a: "))
+    c = int(input("Masukkan panjang c: "))
+    if (c < a):
+        print("Panjang c tidak valid!")
+        c = int(input("Masukkan panjang c: "))
+
+    b = sqrt(c * c - a * a)
+    print("Panjang sisi b adalah:", round(b))
+
+
