623006a1786f3a2dc4a2eb6936efe0d9eaac5883
Muhammad Azka Aulia <88353426+UlrichStern9@users.noreply.github.com>
2021-10-06 09:32:20 +0700

    Update while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -1,3 +1,8 @@
+"""
+Nama: Muhammad Azka Aulia
+NIM: 064001900026
+"""
+
 masuk = [int(input("Masukkan angka awal: ")), int(input("Masukkan angka akhir: "))]
 
 while masuk[0] and masuk[0] <= masuk[1]:
