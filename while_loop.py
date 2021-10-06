44da6def0b91d1efe7e1969939904b88e8913fdf
Muhammad Azka Aulia <88353426+UlrichStern9@users.noreply.github.com>
2021-10-06 09:31:49 +0700

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,6 @@
+masuk = [int(input("Masukkan angka awal: ")), int(input("Masukkan angka akhir: "))]
+
+while masuk[0] and masuk[0] <= masuk[1]:
+    print(masuk[0], masuk[1])
+    masuk[0] += 1
+    masuk[1] -= 1
