3f2d74cb2aba2fb02ad6aee7f210c742264d9770
Muhammad Azka Aulia <88353426+UlrichStern9@users.noreply.github.com>
2021-12-22 09:09:10 +0700

    Update Prak10.py

--- Prak10.py
+++ Prak10.py
@@ -27,7 +27,8 @@ def view_data():
     print("")
 
 
-def hitung_rerata():
+def hitung_rerata_mhs():
+    print("[3. HITUNG RATA-RATA PRAK TIAP MAHASISWA]")
     nm = len(list_nilai)
     temp_rerata = [0] * nm
     for i in range(nm):
@@ -42,25 +43,85 @@ def hitung_rerata():
         print(nama, "=", rata2)
     print("")
 
+
+def hitung_rerata_prak():
+    print("[4. HITUNG RATA-RATA TIAP PRAK]")
+    nm = len(list_nilai)
+    nilai_prak = [0, 0, 0]
+
+    for i in range(nm):
+        nilai = list_nilai[i]
+        np = len(nilai)
+        for j in range(np):
+            if j == 0:
+                nilai_prak[0] += nilai[j]
+            elif j == 1:
+                nilai_prak[1] += nilai[j]
+            else:
+                nilai_prak[2] += nilai[j]
+
+    for i in range(len(nilai_prak)):
+        print("Rerata Prak {}=".format(i + 1), nilai_prak[i] / nm)
+    print("")
+
+
+def cari_rerata_prak_mhs():
+    print("[5. MENCARI NILAI RATA-RATA PRAK TIAP MAHASISWA]")
+    nama_mhs = input("Masukkan nama mahasiswa: ").capitalize()
+    if nama_mhs in list_nama:
+        indeks = list_nama.index(nama_mhs)
+        print("Rerata nilai praktikum {} =".format(nama_mhs), sum(list_nilai[indeks]) / len(list_nilai[indeks]), "\n")
+
+
+def update_nilai_prak():
+    print("[6. UPDATE NILAI PRAK MAHASISWA]")
+    nama_mhs = input("Masukkan nama mahasiswa: ").capitalize()
+    if nama_mhs in list_nama:
+        indeks = list_nama.index(nama_mhs)
+        prak_ke = int(input("Ingin update nilai praktikum ke-: "))
+        if 0 < prak_ke < 4:
+            nilai_baru = int(input("Nilai baru: "))
+            list_nilai[indeks][prak_ke - 1] = nilai_baru
+            print("")
+
+
 def mulai():
     while True:
-        pilihan = input(
-            "PROGRAM LIST\n1. Input Data\n2. View Data\n3. Hitung Nilai Rata-Rata Praktikum Tiap Mahasiswa\nPilih menu "
-            "yang "
-            "tersedia: ")
+        pilihan = input("PROGRAM LIST\n"
+                        "1. Input Data\n"
+                        "2. View Data\n"
+                        "3. Hitung Nilai Rata-Rata Praktikum Tiap Mahasiswa\n"
+                        "4. Hitung Nilai Rata-Rata Tiap Praktikum\n"
+                        "5. Mencari Nilai Rata-Rata Praktikum Mahasiswa\n"
+                        "6. Update Nilai Praktikum Mahasiswa\n"
+                        "7. Exit\n"
+                        "Pilih menu yang tersedia: ")
         print("")
-    
+
         if pilihan == "1":
             input_data()
+
         elif pilihan == "2":
             view_data()
+
         elif pilihan == "3":
-            hitung_rerata()
+            hitung_rerata_mhs()
+
+        elif pilihan == "4":
+            hitung_rerata_prak()
+
+        elif pilihan == "5":
+            cari_rerata_prak_mhs()
+
+        elif pilihan == "6":
+            update_nilai_prak()
+
         elif pilihan == "7":
             print("[7. EXIT]\nTERIMA KASIH!")
             break
         else:
-            print("Pilih 1, 2, 3, 7 untuk keluar\n")
+            print("Pilih 1, 2, 3, 4, 5, 6 atau 7 untuk keluar\n")
+
 
 if __name__ == "__main__":
     mulai()
