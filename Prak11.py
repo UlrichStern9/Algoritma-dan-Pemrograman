0a7dd36b029bb25a176b882c8fe2dae3c3efa9ec
Muhammad Azka Aulia <88353426+UlrichStern9@users.noreply.github.com>
2021-12-23 16:38:05 +0700

    Update Prak11.py

--- Prak11.py
+++ Prak11.py
@@ -3,24 +3,17 @@ Nama: Muhammad Azka Aulia
 NIM: 064001900026
 """
 
-list_nama = []
-list_nilai = []
+nama_file = ""
+temp_update = ""
+indeks = 0
 
 
-def buka_file():
+def cek_file():
+    global nama_file
     while True:
         nama_file = input("Masukkan nama file: ")
         try:
-            file = open(nama_file)
-            for row in range(5):
-                temp_nilai = []
-                data_nilai = file.readline()
-                temp_nilai.append(data_nilai.split())
-                nilai = temp_nilai.pop()
-                list_nama.append(nilai.pop(0))
-                list_nilai.append(nilai)
-                konversi_int = [int(item) for item in list_nilai[row]]
-                list_nilai[row] = konversi_int
+            open(nama_file)
             print("")
             break
         except FileNotFoundError:
@@ -29,30 +22,66 @@ def buka_file():
 
 def baca_data():
     print("[1. BACA DATA]\n NAMA | PRAK 1 | PRAK 2 | PRAK 3\n--------------------------------")
-    for nama, nilai in zip(list_nama, list_nilai):
-        prak1, prak2, prak3 = nilai
-        print(nama, "\t", prak1, "\t", prak2, "\t", prak3)
-    print("")
+    baca_data1 = open(nama_file)
+    print(baca_data1.read())
 
 
 def cari_rerata_prak_mhs():
     print("[2. MENCARI RATA-RATA NILAI PRAK TIAP MAHASISWA]")
-    nama_mhs = input("Masukkan nama mahasiswa: ").capitalize()
-    if nama_mhs in list_nama:
-        indeks = list_nama.index(nama_mhs)
-        print(" NAMA | PRAK 1 | PRAK 2 | PRAK 3\n--------------------------------")
-        prak1, prak2, prak3 = list_nilai[indeks]
-        print(nama_mhs, "\t", prak1, "\t", prak2, "\t", prak3)
-        print("\nRerata nilai praktikum {} =".format(nama_mhs), sum(list_nilai[indeks]) / len(list_nilai[indeks]), "\n")
+    data_nilai = []
+    baca_data2 = open(nama_file).readlines()
+    nama = input("Masukkan nama mahasiswa: ").capitalize()
+    for nilai in range(len(baca_data2)):
+        data_nilai.append(baca_data2[nilai].split())
+        if nama in data_nilai[nilai][0]:
+            prak1, prak2, prak3 = int(data_nilai[nilai][1]), int(data_nilai[nilai][2]), int(data_nilai[nilai][3])
+            print(" NAMA | PRAK 1 | PRAK 2 | PRAK 3\n--------------------------------")
+            print("{} \t {} \t {} \t {}\n".format(nama, prak1, prak2, prak3))
+            print("Rerata nilai praktikum {} =".format(nama), (prak1 + prak2 + prak3) / 3, "\n")
+            break
+
+
+def update_nilai_prak():
+    global temp_update, indeks
+    print("[3. UPDATE NILAI PRAK MAHASISWA]")
+    data_nilai = []
+    baca_data3 = open(nama_file).readlines()
+    nama = input("Masukkan nama mahasiswa: ").capitalize()
+    for nilai in range(len(baca_data3)):
+        data_nilai.append(baca_data3[nilai].split())
+        if nama in data_nilai[nilai][0]:
+            indeks = nilai
+            prak_ke = int(input("Ingin update nilai praktikum ke-: "))
+            if 0 < prak_ke < 4:
+                nilai_baru = input("Nilai baru: ")
+                data_nilai[nilai][prak_ke] = nilai_baru
+                prak1, prak2, prak3 = data_nilai[nilai][1], data_nilai[nilai][2], data_nilai[nilai][3]
+                temp_update = "{} \t {} \t {} \t {}\n".format(nama, prak1, prak2, prak3)
+                print("")
+                break
+
+
+def simpan_perubahan():
+    print("[4. SIMPAN UPDATE NILAI]")
+    baca_data4 = open("Data Nilai.txt", "r")
+    data_nilai = baca_data4.readlines()
+    data_nilai[indeks] = temp_update
+
+    baca_data4 = open("Data Nilai.txt", "w")
+    baca_data4.writelines(data_nilai)
+    baca_data4.close()
+    print("PERUBAHAN BERHASIL DISIMPAN\n")
 
 
 def mulai():
-    buka_file()
+    cek_file()
 
     while True:
         pilihan = input("MENU\n"
                         "1. Baca Data\n"
-                        "2. Mencari Rata-Rata Nilai Praktikum Mahasiswa\n"
+                        "2. Mencari Nilai Rata-Rata Praktikum Mahasiswa\n"
+                        "3. Update Nilai Praktikum Mahasiswa\n"
+                        "4. Simpan Perubahan Nilai\n"
                         "5. Exit\n"
                         "Pilih menu yang tersedia: ")
         print("")
@@ -63,11 +92,17 @@ def mulai():
         elif pilihan == "2":
             cari_rerata_prak_mhs()
 
+        elif pilihan == "3":
+            update_nilai_prak()
+
+        elif pilihan == "4":
+            simpan_perubahan()
+
         elif pilihan == "5":
             print("[5. EXIT]\nTERIMA KASIH!")
             break
         else:
-            print("Pilih 1, 2, atau 5 untuk keluar\n")
+            print("Pilih 1, 2, 3, 4 atau 5 untuk keluar\n")
 
 
 if __name__ == "__main__":
